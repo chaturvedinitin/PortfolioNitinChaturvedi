d68bd4f2bc5c43d437124db8ffc0d4a9dca39e35
Nitin Chaturvedi <chaturvediinitin@gmail.com>
2025-07-30 14:11:14 +0530

    temp

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Introduction from './Introduction'
 const Home = () => {
   return (
-    <div className='container mx-auto max-w-4xl mt-8 px-12'>
+    <div className='container mx-auto max-w-4xl mt-8 sm:px-0 md:px-4 lg:px-12'>
       <Introduction />
     </div>
   )
